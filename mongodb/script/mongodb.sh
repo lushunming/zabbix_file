171184a51a7ff3fd5ccc20cc3bced2a11d2bd079
lushunming <1357197829@qq.com>
2017-06-27 14:10:35 +0800

    Update mongodb.sh

--- mongodb/script/mongodb.sh
+++ mongodb/script/mongodb.sh
@@ -1,11 +1,9 @@
-  #!/bin/bash
-    index=$(echo $@ | tr " " ".")
-
-    status=$(echo "db.serverStatus().${index}" |/usr/local/mongodb/bin/mongo   admin |sed -n '3p')
-
-    #check if the output contains "NumberLong"
-    if [[ "$status" =~ "NumberLong"   ]];then
+#!/bin/bash
+index=$(echo $@ | tr " " ".")
+status=$(echo "db.serverStatus().${index}" |/usr/bin/mongo  --quiet admin)
+#check if the output contains "NumberLong"
+if [[ "$status" =~ "NumberLong" ]];then
             echo $status|sed -n 's/NumberLong(//p'|sed -n 's/)//p'
-    else
+else
             echo $status
-    fi
+fi
